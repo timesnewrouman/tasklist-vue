3579c248dd4796552be749a274c9b31ec1b2aba4
Roman Zhigaikov <zhigaikov.roman@yandex.ru>
2020-09-16 19:37:12 +0300

    version 1.2.0

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,28 +1,32 @@
 <template>
   <div>
     <ul>
-      <Task
-        v-for="(task, i) of tasks"
-        v-bind:task="task"
-        v-bind:index="i"
-        @remove-task="removeTask"
-      />
+      <li v-for="task in tasks" v-bind:task="task" v-bind:key="task.id">
+        <span v-bind:class="{ done: task.completed }">
+          <input
+            type="checkbox"
+            @change="task.completed = !task.completed"
+            v-bind:checked="task.completed"
+          />
+          <strong>{{ task.id }}</strong>
+          {{ task.title }}
+        </span>
+        <button class="remove" @click="$emit('remove-task', task.id)"></button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import Task from "./Task";
-
 export default {
   name: "TaskList",
-  props: ["tasks"],
-  components: {
-    Task,
-  },
-  methods: {
-    removeTask(id) {
-      this.$emit("remove-task", id);
+  props: {
+    tasks: {
+      task: {
+        type: Object,
+        required: true,
+        id: Number,
+      },
     },
   },
 };
@@ -31,5 +35,30 @@ export default {
 <style scoped>
 ul {
   padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+}
+
+input {
+  margin-right: 10px;
+}
+
+.remove {
+  background-image: url(../images/delete-icon.png);
+  width: 24px;
+  height: 24px;
+  padding: 0;
+  border: none;
+  background-color: white;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
